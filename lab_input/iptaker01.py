86c901f20f57c18a81c2ae49eac250944c777638
Marco Bragado <marcobragado.cs@gmail.com>
2021-03-29 10:53:57 -0700

    added vendorName input

--- lab_input/iptaker01.py
+++ lab_input/iptaker01.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
 user_input = input("Please enter an IPv4 IP address:")
 print("You told me the IPv4 address is:" + user_input)
+vendorName = input("What is the 'vendor name' of your device?")
+print("The vendor name is " + vendorName)
