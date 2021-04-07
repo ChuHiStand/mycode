2c7fab82cf6cdb6e28836bc3a43bcf88910d5e71
Marco Bragado <marcobragado.cs@gmail.com>
2021-04-07 15:20:28 +0000

    google_challenge added

---
+++ challenges/google_challenge.py
@@ -0,0 +1,20 @@
+#!user/bin/env python3
+
+import os
+import subprocess
+
+
+
+"""Google Challenge"""
+
+
+main():
+    os.system("sudo apt install sl")
+    os.system("/usr/games/sl")
+    os.system("mkdir slappy")
+    os.system("touch slappy/chad_stop_using_that_word.txt")
+
+
+
+
+main()
